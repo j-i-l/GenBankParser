f0341e4708abcfe375f671e1713f9798c8e525e3
Jonas I Liechti <jonas.i.liechti@protonmail.ch>
2018-10-11 15:03:00 +0200

    importing from setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, unicode_literals
 
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='GenbankParser',
